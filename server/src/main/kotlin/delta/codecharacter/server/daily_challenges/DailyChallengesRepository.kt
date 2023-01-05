9037a5df1ab6f3b6bed40be3b8b35ee715a31dc8
b1otch <vvivek1801@gmail.com>
2023-01-05 07:12:29 +0530

    fix(Removed Unnecessary Files): Removed Entity and Repository files

--- server/src/main/kotlin/delta/codecharacter/server/daily_challenges/DailyChallengesRepository.kt
+++
@@ -1,6 +0,0 @@
-package delta.codecharacter.server.daily_challenges
-
-import org.springframework.data.mongodb.repository.MongoRepository
-import java.util.UUID
-
-interface DailyChallengesRepository : MongoRepository<DailyChallengeEntity, UUID>
