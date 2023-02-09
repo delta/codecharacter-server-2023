d6fd545984c52204c3679915492501ddf201ddf6
https://github.com/Valliammai-SM <112121074@nitt.edu>
2023-02-09 17:29:56 +0530

    fix: formatting

--- server/src/main/kotlin/delta/codecharacter/server/schedulers/LeaderboardScheduling.kt
+++ server/src/main/kotlin/delta/codecharacter/server/schedulers/LeaderboardScheduling.kt
@@ -2,14 +2,11 @@ package delta.codecharacter.server.schedulers
 
 import delta.codecharacter.server.user.public_user.PublicUserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 class LeaderboardScheduling(@Autowired private val publicUserService: PublicUserService) {
-    @Value("\${environment.registration-time}") val registrationDate: String = ""
-    @Value("\${environment.game-start-time}") val eventDate: String = ""
 
     @Scheduled(cron = "\${environment.registration-time}")
     fun updateTempLeaderboard() {
